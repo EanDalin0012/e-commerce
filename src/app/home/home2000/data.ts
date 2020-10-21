b7f104f270fbd3fd985064deb360b3b283562b91
Ean Dalin <eandalined@gmail.com>
2020-10-21 22:35:18 +0700

    commit

--- src/app/home/home2000/data.ts
+++ src/app/home/home2000/data.ts
@@ -12,7 +12,7 @@ export const data: Product[] =[
     },{
       id: 2,
       name: 'Product 2',
-      src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+      src: 'https://www.envary.com/wp-content/uploads/2018/07/Envary-Angel-Shenzhen_CWS-CWP-Water-Purifier-series-industrial-design_product-family_1600_09_78q-768x500.jpg',
       description: 'Testing product 2',
       price: 20,
       currency: 'USD',
@@ -20,7 +20,7 @@ export const data: Product[] =[
     },{
       id: 3,
       name: 'Product 3',
-      src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+      src: 'https://i.pinimg.com/236x/de/04/05/de0405bda074c59c694fb8f4650b6e2f--product-design-grip.jpg',
       description: 'Testing product 3',
       price: 10,
       currency: 'USD',
@@ -28,7 +28,7 @@ export const data: Product[] =[
     },{
       id: 4,
       name: 'Product 4',
-      src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+      src: 'https://cdn.lynda.com/course/636119/636119-637286227554169414-16x9.jpg',
       description: 'Testing product 4',
       price: 15,
       currency: 'USD',
@@ -36,7 +36,7 @@ export const data: Product[] =[
     },{
       id: 5,
       name: 'Product 5',
-      src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+      src: 'https://3.imimg.com/data3/XO/YL/MY-15402974/3d-product-design-500x500.jpg',
       description: 'Testing product 5',
       price: 20,
       currency: 'USD',
@@ -44,7 +44,7 @@ export const data: Product[] =[
     },{
       id: 6,
       name: 'Product 6',
-      src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+      src: 'https://blogs.autodesk.com/design-studio/wp-content/uploads/sites/136/2019/11/Sewing_Machine_Render-1024x724.jpg',
       description: 'Testing product 6',
       price: 10,
       currency: 'USD',
@@ -52,7 +52,7 @@ export const data: Product[] =[
     },{
         id: 7,
         name: 'Product 7',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://lh3.googleusercontent.com/proxy/NRPyna-mj7Y1qBunO1wa18JsuwGX5eabIV3eKt9vpJyS79-9-m_nKk4-vY9nOA4i3gX21OFhyXfQ3XrOIP7a8gqiJ3C_FV6S-OWzHOTKIOiAhYE4yn0jTtgwovRhO9k2soI',
         description: 'Testing product 7',
         price: 20,
         currency: 'USD',
@@ -60,7 +60,7 @@ export const data: Product[] =[
       },{
         id: 8,
         name: 'Product 8',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://uploads.toptal.io/blog/image/122344/toptal-blog-image-1488472779148-45a454fd36e1b05f6dfac62d2efd84f5.jpeg',
         description: 'Testing product 8',
         price: 10,
         currency: 'USD',
@@ -68,7 +68,7 @@ export const data: Product[] =[
       },{
         id: 1,
         name: 'Product 1',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://technabob.com/blog/wp-content/uploads/2011/12/Easy-Iron.jpg',
         description: 'Testing product 1',
         price: 20,
         currency: 'USD',
@@ -76,7 +76,7 @@ export const data: Product[] =[
       },{
         id: 2,
         name: 'Product 2',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://www.yankodesign.com/images/design_news/2011/09/08/iron7.jpg',
         description: 'Testing product 2',
         price: 20,
         currency: 'USD',
@@ -84,7 +84,7 @@ export const data: Product[] =[
       },{
         id: 3,
         name: 'Product 3',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://professional3dservices.com/img/modeling/electronic-3d-model.jpg',
         description: 'Testing product 3',
         price: 10,
         currency: 'USD',
@@ -92,7 +92,7 @@ export const data: Product[] =[
       },{
         id: 4,
         name: 'Product 4',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://designapplause.com/wp-content/xG58hlz9/2010/11/kravis3-14b.png',
         description: 'Testing product 4',
         price: 15,
         currency: 'USD',
@@ -100,7 +100,7 @@ export const data: Product[] =[
       },{
         id: 5,
         name: 'Product 5',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://designrfix.com/wp-content/uploads/2009/12/industrial-design-2009-products-45.jpg',
         description: 'Testing product 5',
         price: 20,
         currency: 'USD',
@@ -108,7 +108,7 @@ export const data: Product[] =[
       },{
         id: 6,
         name: 'Product 6',
-        src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+        src: 'https://www.yankodesign.com/images/design_news/2020/08/auto-draft/bmw_r100_good_ghost_1-510x340.jpg',
         description: 'Testing product 6',
         price: 10,
         currency: 'USD',
@@ -116,7 +116,7 @@ export const data: Product[] =[
       },{
           id: 7,
           name: 'Product 7',
-          src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+          src: 'https://inspgr.id/app/uploads/2016/03/art-ron-arad-feature.jpg',
           description: 'Testing product 7',
           price: 20,
           currency: 'USD',
@@ -124,7 +124,7 @@ export const data: Product[] =[
         },{
           id: 8,
           name: 'Product 8',
-          src: 'https://www.telerik.com/kendo-angular-ui-develop/components/layout/examples/card/display_media/black_sea.jpg',
+          src: 'https://lh3.googleusercontent.com/proxy/MbUDZ3QncyIEJ8wFqeNujoogsDLtFduuXf8jm-H90a3izkLH_bXhreCYXFI9Ty82CNgw-tmcz70W1QMRferUDgygKigCmG5sqggv0QVb63iz4Qdz36Gr',
           description: 'Testing product 8',
           price: 10,
           currency: 'USD',
